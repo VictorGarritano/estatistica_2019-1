863239ddcbf67c42297ac222d044a18cfe0945e2
Victor Garritano <victortaichou@gmail.com>
2019-03-22 20:48:55 -0300

    running linters

--- poker.py
+++ poker.py
@@ -2,15 +2,13 @@
 
 import json
 from multiprocessing import Pool
-from datetime import datetime
 from collections import defaultdict
 from deuces import Deck
 from deuces import Evaluator
-from deuces import Card
 
 
-sample_size = 100000
-runs = 1000
+SAMPLE_SIZE = 100000
+RUNS = 1000
 
 
 def run(number):
@@ -26,11 +24,11 @@ def run(number):
         - stats (str): returns a JSON formatted string containing
             probabilities for each poker hand
     """
-    print('starting run #{0}'.format(number))
+    print 'starting run #{0}'.format(number)
     evaluator = Evaluator()
     poker_hands = defaultdict(int)
 
-    for i in range(sample_size):
+    for _ in range(SAMPLE_SIZE):
         deck = Deck()
         p1_hand = deck.draw(5)
         p1_score = evaluator.evaluate(p1_hand, [])
@@ -40,13 +38,13 @@ def run(number):
             p1_class = evaluator.get_rank_class(p1_score)
             poker_hands[evaluator.class_to_string(p1_class)] += 1
 
-    stats = dict((k, round(float(v)/sample_size, 7))
+    stats = dict((k, round(float(v)/SAMPLE_SIZE, 7))
                  for k, v in poker_hands.items())
     return json.dumps(stats)
 
 if __name__ == '__main__':
-    p = Pool(7)
-    result = p.map(run, range(runs))
+    PROCESSING_POOL = Pool(7)
+    RESULT = PROCESSING_POOL.map(run, range(RUNS))
 
     with open('data.json', 'w') as outfile:
-        json.dump(result, outfile, sort_keys=True, indent=4)
+        json.dump(RESULT, outfile, sort_keys=True, indent=4)
